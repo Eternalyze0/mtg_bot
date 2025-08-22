20c10dd8989b4f0193e36fc726b09ef584160fef
Eternalyze0 <eternalyze0@gmail.com>
2025-08-22 12:14:05 +0700

    Add files via upload

--- mtg_bot.py
+++ mtg_bot.py
@@ -1,11 +1,14 @@
 import json
 
+with open('run_sim_cmd.txt') as f:
+	run_sim_cmd = f.read()
+
 f_name = 'oracle-cards-20250821090338.json'
 
 with open(f_name) as f:
 	cards = json.loads(f.read())
 
-standard_cards = [card for card in cards if card['legalities']['standard'] == 'legal']
+standard_cards = [card for card in cards if card['legalities']['standard'] == 'legal' and not '//' in card['name']]
 
 basic_land_names = ['Swamp', 'Island', 'Forest', 'Mountain', 'Plains']
 
@@ -24,6 +27,22 @@ def scryfall_to_forge(scryfall_deck, name='Test Deck'):
 		else:
 			forge_deck[forge_card] = 1
 	forge_deck_string = '[metadata]' + '\nName=' + name + '\n[Main]\n'
+	for card, count in sorted(forge_deck.items(), key=lambda x: x[0]):
+		forge_deck_string += str(count) + ' ' + card + '\n'
+	return forge_deck_string
+
+def format_scryfall_deck(scryfall_deck):
+	forge_deck = {}
+	for scryfall_card in scryfall_deck:
+		forge_card = scryfall_card['name']
+		# if scryfall_card['name'] in basic_land_names:
+		# 	forge_card += '|1'
+		if forge_card in forge_deck:
+			forge_deck[forge_card] += 1
+		else:
+			forge_deck[forge_card] = 1
+	# forge_deck_string = '[metadata]' + '\nName=' + name + '\n[Main]\n'
+	forge_deck_string = 'Deck\n'
 	for card, count in sorted(forge_deck.items(), key=lambda x: x[0]):
 		forge_deck_string += str(count) + ' ' + card + '\n'
 	return forge_deck_string
@@ -68,7 +87,7 @@ import os
 learning_rate = 0.0005
 gamma         = 0.98
 buffer_limit  = 50000
-batch_size    = 32
+batch_size    = 1 #32
 
 
 class ForgeEnv(gym.Env):
@@ -131,11 +150,13 @@ class Qnet(nn.Module):
 		super(Qnet, self).__init__()
 		self.fc1 = nn.Linear(n_actions, 128) # state should be the cards cards already in the deck
 		self.fc2 = nn.Linear(128, 128)
+		self.do = nn.Dropout(0.5)
 		self.fc3 = nn.Linear(128, n_actions)
  
 	def forward(self, x):
 		x = F.relu(self.fc1(x))
 		x = F.relu(self.fc2(x))
+		x = self.do(x)
 		x = self.fc3(x)
 		return x
 	  
@@ -166,19 +187,19 @@ def main():
 	env = ForgeEnv()
 	env2 = ForgeEnv()
 	q = Qnet()
-	q2 = Qnet()
+	# q2 = Qnet() # only need 1 agent
 	q_target = Qnet()
 	q_target.load_state_dict(q.state_dict())
-	q2_target = Qnet()
-	q2_target.load_state_dict(q2.state_dict())
+	# q2_target = Qnet()
+	# q2_target.load_state_dict(q2.state_dict())
 	memory = ReplayBuffer()
 
 	print_interval = 20
 	score = 0.0  
 	optimizer = optim.Adam(q.parameters(), lr=learning_rate)
-	optimizer2 = optim.Adam(q2.parameters(), lr=learning_rate)
+	# optimizer2 = optim.Adam(q2.parameters(), lr=learning_rate)
 
-	for n_epi in range(1):
+	for n_epi in range(100):
 		epsilon = max(0.01, 0.08 - 0.01*(n_epi/200)) #Linear annealing from 8% to 1%
 		s = env.reset()
 		done = False
@@ -193,25 +214,39 @@ def main():
 			done_mask = 0.0 if done else 1.0
 
 
-			a_dist2 = q2(torch.from_numpy(s2).float()) * torch.from_numpy(env2.get_4x_mask())
+			a_dist2 = q(torch.from_numpy(s2).float()) * torch.from_numpy(env2.get_4x_mask())
 			a2 = a_dist2.argmax()     
 			s_prime2, r2, done2, truncated2, info2 = env2.step(a2)
 			done_mask2 = 0.0 if done2 else 1.0
 
-			assert q.fc1.weight[0][0] != q2.fc1.weight[0][0]
+			# assert q.fc1.weight[0][0] != q2.fc1.weight[0][0]
+
+			# print(a_dist)
+			# print(a_dist2)
+
+			assert a_dist[0] != a_dist2[0]
 
 			if done and done2:
 				# RUN FORGE_AI SIMULATION HERE TO COMPUTE REWARDS
-				with open('deck1.dck', 'w') as f:
-					f.write(scryfall_to_forge(env.deck))
-				with open('deck2.dck', 'w') as f:
-					f.write(scryfall_to_forge(env2.deck))
-				# os.system('java -jar forge.jar sim -d deck1.dck deck2.dck -n 1')
-				coin = random.random() # simulate with a coin for now
-				if coin < 0.5:
+				with open('./decks/deck1.dck', 'w') as f:
+					f.write(scryfall_to_forge(env.deck, name='0'))
+				with open('./decks/deck2.dck', 'w') as f:
+					f.write(scryfall_to_forge(env2.deck, name='1'))
+				os.system(run_sim_cmd)
+				# os.system('java -jar forge.jar sim -d deck1.dck deck2.dck')
+				# coin = random.random() # simulate with a coin for now
+				# if coin < 0.5:
+				with open('output.txt') as f:
+					output = f.read()
+				if '0 has won' in output:
+					print('0 has won')
 					r, r2 = 1, -1
-				else:
+				elif '1 has won' in output:
+					print('1 has won')
 					r, r2 = -1, 1
+				else:
+					print('Neither player won, exiting..')
+					exit()
 
 			memory.put((s, a, r/100.0, s_prime, done_mask))
 			memory.put((s2, a2, r2/100.0, s_prime2, done_mask2))
@@ -226,15 +261,22 @@ def main():
 			
 		if memory.size()>=batch_size:
 			train(q, q_target, memory, optimizer)
-			train(q2, q2_target, memory, optimizer2)
+			# train(q2, q2_target, memory, optimizer2)
 
 		if n_epi%print_interval==0 and n_epi!=0:
 			q_target.load_state_dict(q.state_dict())
-			q2_target.load_state_dict(q2.state_dict())
+			# q2_target.load_state_dict(q2.state_dict())
 			print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
 															n_epi, score/print_interval, memory.size(), epsilon*100))
 			score = 0.0
+	
+	print('FINAL DECKS:')
+	print()
+	print(format_scryfall_deck(env.deck))
+	print(format_scryfall_deck(env2.deck))
+
 	env.close()
+	env2.close()
 
 if __name__ == '__main__':
 	main()
